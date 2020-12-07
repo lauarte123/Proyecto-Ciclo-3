33dcdb03b415deb0505bff931bd513bb63531a2e
Laura Arteaga <laarteaga@uninorte.edu.co>
2020-12-06 19:00:12 -0500

    Validación contraseña v1

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import yagmail as yagmail
+import utils
 
 app = Flask(__name__)
 
+#Activación de cuenta
 @app.route("/")
 def principal():
+    if request.method == 'POST':
+        username = request.form.get('usuario')
+        password = request.form['password']
+        email = request.form.get('correo')
+
+        if not utils.isUsernameValid(username):
+            return render_template('Cover.html')
+
+        if not utils.isPasswordValid(password):
+            return render_template('Cover.html')
+        
+        if not utils.isEmailValid(email):
+            return render_template('Cover.html')
+
+        #El correo de donde la aplicación envía el correo
+        yag = yagmail.SMTP('laarteaga@uninorte.edu.co','13uninorte31')
+        yag.send(to=email, subject='Activa tu cuenta', contents="Bienvenido, usa el siguiente link para activar tu cuenta")
+        return redirect('/perfil')
+
+
     return render_template('Cover.html')
+
+
 
 @app.route("/perfil")
 def perfil():
