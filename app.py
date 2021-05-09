24356aa72e32347d3d99e263f53d5a4c8d08491b
feeldiac <feeldiac@gmail.com>
2021-05-09 02:04:02 -0500

    Perfil: creando archivos iniciales

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import sqlite3
 from sqlite3 import Error
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
-from livereload import Server
+
 
 app = Flask(__name__)
 app.secret_key = "D*G-KaPdSgVkYp3s5v8y/B?E(H+MbQeT"
@@ -35,7 +35,13 @@ def refact1():
 
 @app.route('/refact2')
 def refact2():
-    return render_template('portada2.html')    
+    # return render_template('portada2.html')   
+    return render_template('Profile2.html')   
+
+@app.route('/refact3')
+def refact3():
+    return render_template('Profile2.html') 
+
 
 @app.route('/')
 def principal():
@@ -525,5 +531,3 @@ def downloadimage():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    server = Server(app.wsgi_app)
-    server.serve()
